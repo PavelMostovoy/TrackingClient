d3e5bb7c5bdf6bc46e0f962ab5bb7b82c2c0ef04
Pavel Mostovoy <pavelmostovoy@gmail.com>
2021-05-17 20:58:15 +0300

    added second screen

--- app/src/main/java/com/mostovoi/trackingclient/AddObjectActivity.kt
+++ app/src/main/java/com/mostovoi/trackingclient/AddObjectActivity.kt
@@ -1,11 +1,29 @@
 package com.mostovoi.trackingclient
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_add_object.*
 
 class AddObjectActivity : AppCompatActivity() {
+
+    /**
+     * This function is auto created by Android when the Activity Class is created.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
+        //This call the parent constructor
         super.onCreate(savedInstanceState)
+
+        // This is used to align the xml view to this class
         setContentView(R.layout.activity_add_object)
+
+        // TODO (Step 2: Setting up the action bar using the toolbar and making enable the home back button and also adding the click of it.)
+        // START
+        setSupportActionBar(toolbar_add_object) // Use the toolbar to set the action bar.
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // This is to use the home back button.
+        // Setting the click event to the back button
+        toolbar_add_object.setNavigationOnClickListener {
+            onBackPressed()
+        }
+        // END
     }
 }
