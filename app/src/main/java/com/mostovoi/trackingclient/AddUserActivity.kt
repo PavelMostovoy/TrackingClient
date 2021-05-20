56ab613bc90f65600b823b628a5e650d32c59303
Pavel Mostovoy <pavelmostovoy@gmail.com>
2021-05-20 10:56:29 +0300

    cleanup

--- app/src/main/java/com/mostovoi/trackingclient/AddUserActivity.kt
+++ app/src/main/java/com/mostovoi/trackingclient/AddUserActivity.kt
@@ -2,7 +2,6 @@ package com.mostovoi.trackingclient
 
 
 import android.Manifest
-import android.app.DatePickerDialog
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
@@ -18,21 +17,9 @@ import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import kotlinx.android.synthetic.main.activity_add_user.*
-import java.text.SimpleDateFormat
 import java.util.*
 
 class AddUserActivity : AppCompatActivity(), View.OnClickListener {
-
-    /**
-     * An variable to get an instance calendar using the default time zone and locale.
-     */
-    private var cal = Calendar.getInstance()
-
-    /**
-     * A variable for DatePickerDialog OnDateSetListener.
-     * The listener used to indicate the user has finished selecting a date. Which we will be initialize later on.
-     */
-    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
 
     /**
      * This function is auto created by Android when the Activity Class is created.
@@ -51,44 +38,17 @@ class AddUserActivity : AppCompatActivity(), View.OnClickListener {
             onBackPressed()
         }
 
-        // https://www.tutorialkart.com/kotlin-android/android-datepicker-kotlin-example/
-        // create an OnDateSetListener
-        dateSetListener =
-            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
-                cal.set(Calendar.YEAR, year)
-                cal.set(Calendar.MONTH, monthOfYear)
-                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-
-                updateDateInView()
-            }
-
-        et_date.setOnClickListener(this)
-        // TODO(Step 1: Adding an onclick listener to tv_add_image)
-        // START
         tv_add_image.setOnClickListener(this)
-        // END
+
     }
 
     override fun onClick(v: View?) {
         when (v!!.id) {
-            R.id.et_date -> {
-                DatePickerDialog(
-                    this@AddUserActivity,
-                    dateSetListener, // This is the variable which have created globally and initialized in setupUI method.
-                    // set DatePickerDialog to point to today's date when it loads up
-                    cal.get(Calendar.YEAR), // Here the cal instance is created globally and used everywhere in the class where it is required.
-                    cal.get(Calendar.MONTH),
-                    cal.get(Calendar.DAY_OF_MONTH)
-                ).show()
-            }
-
-            // TODO(Step 2 : Adding an alert dialog for selection of image.)
-            // START
             R.id.tv_add_image -> {
                 val pictureDialog = AlertDialog.Builder(this)
                 pictureDialog.setTitle("Select Action")
                 val pictureDialogItems =
-                    arrayOf("Select photo from gallery", "Capture photo from camera","Get Location")
+                    arrayOf("Select photo from gallery", "Capture photo from camera")
                 pictureDialog.setItems(
                     pictureDialogItems
                 ) { _, which ->
@@ -96,7 +56,6 @@ class AddUserActivity : AppCompatActivity(), View.OnClickListener {
                         // Here we have create the methods for image selection from GALLERY
                         0 -> choosePhotoFromGallery()
                         1 -> Toast.makeText(this@AddUserActivity,"Camera selection coming soon...", Toast.LENGTH_SHORT).show()
-                        2 -> Toast.makeText(this@AddUserActivity,"Location will be requested later",Toast.LENGTH_LONG).show()
                     }
                 }
                 pictureDialog.show()
@@ -106,28 +65,15 @@ class AddUserActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     /**
-     * A function to update the selected date in the UI with selected format.
-     * This function is created because every time we don't need to add format which we have added here to show it in the UI.
-     */
-    private fun updateDateInView() {
-        val myFormat = "dd.MM.yyyy" // mention the format you need
-        val sdf = SimpleDateFormat(myFormat, Locale.getDefault()) // A date format
-        et_date.setText(sdf.format(cal.time).toString()) // A selected date using format which we have used is set to the UI.
-    }
-
-    // TODO (Step 3 : Creating a method for image selection from GALLERY / PHOTOS of phone storage.)
-    // START
-    /**
      * A method is used for image selection from GALLERY / PHOTOS of phone storage.
      */
-    private fun choosePhotoFromGallery() {
-        // TODO(Step 6 : Asking the permissions of Storage using DEXTER Library which we have added in gradle file.)
-        // START
-        Dexter.withActivity(this)
+    private fun choosePhotoFromGallery() =
+    // Asking the permissions of Storage using DEXTER Library which we have added in gradle file
+        Dexter.withContext(this)
             .withPermissions(
                 Manifest.permission.READ_EXTERNAL_STORAGE,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.ACCESS_FINE_LOCATION
+//                Manifest.permission.ACCESS_FINE_LOCATION
             )
             .withListener(object : MultiplePermissionsListener {
                 override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
@@ -147,15 +93,7 @@ class AddUserActivity : AppCompatActivity(), View.OnClickListener {
                 }
             }).onSameThread()
             .check()
-        // END
-    }
-    // END
 
-    // TODO(Step 7: Creating a function which is used to show the alert dialog when the permissions are denied and need to allow it from settings app info.)
-    // START
-    /**
-     * A function used to show the alert dialog when the permissions are denied and need to allow it from settings app info.
-     */
     private fun showRationalDialogForPermissions() {
         AlertDialog.Builder(this)
             .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
@@ -175,5 +113,4 @@ class AddUserActivity : AppCompatActivity(), View.OnClickListener {
                 dialog.dismiss()
             }.show()
     }
-    // END
 }
